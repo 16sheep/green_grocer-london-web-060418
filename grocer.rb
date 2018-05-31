ce46ab179640e913ce9ce646a0941029c05a7f39
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-31 14:20:47 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def consolidate_cart(cart)
   cart.each { |item|
     item.each {|product, attributes|
       if counted_hash.key?(product)
-        counted_hash[product][attributes][:count] += 1
+          counted_hash[product][:count] += 1
         print "value is #{value}"
       else
         counted_hash[product] = attributes
