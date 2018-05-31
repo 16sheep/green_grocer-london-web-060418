526e072c032ac648b71185bc6a057ca88149f02a
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-31 15:07:03 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ end
 
 def apply_clearance(cart)
   cart.map {|item, attributes|
-      if item[:clearance] == TRUE
+      if item[attributes][:clearance] == TRUE
          item[:price] *= 0.2
       end
   }
